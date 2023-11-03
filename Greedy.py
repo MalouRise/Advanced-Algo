bccc620494423affc4f75a3fa03a50a5f5bff0c8
Malouriise <poerani.desselier@hotmail.fr>
2023-11-03 17:01:56 +0100

    Greedy Algo + README

---
+++ Greedy.py
@@ -0,0 +1,48 @@
+def greedy(matrice):
+    if not matrice:
+        return 0, (0, 0, 0, 0)  
+
+    ligne = len(matrice)
+    colonne = len(matrice[0])
+    resultat = 0, (0, 0, 0, 0)  
+    maximum = float('-inf')
+
+    for x in range(colonne):
+        temp = [0] * ligne  
+        for y in range(x, colonne):  
+            for z in range(ligne):
+                temp[z] += matrice[z][y]
+
+            somme = 0
+            debut = 0
+            for z in range(ligne):
+                somme += temp[z]
+                if somme > maximum:
+                    maximum = somme
+                    resultat = maximum, (debut, x, z, y)  
+                if somme < 0:
+                    somme = 0
+                    debut = z + 1
+
+    return resultat  
+
+matrice_A = [
+    [1, 2, -1, -4, -20],
+    [-8, -3, 4, 2, 1],
+    [3, 8, 10, 1, 3],
+    [-4, -1, 1, 7, -6]
+]
+
+maximum, (debut, x, z, y) = greedy(matrice_A)
+print("Somme maximale : ", maximum)
+
+
+sousMatrice = []
+for i in range(debut, z + 1):
+    ligne = []
+    for j in range(x, y + 1):
+        ligne.append(matrice_A[i][j])
+    sousMatrice.append(ligne)
+
+for row in sousMatrice:
+    print(row)
--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# Advanced-Algo
-Cet algorithme brute-force explore systématiquement toutes les sous-matrices possibles pour trouver la sous-matrice de somme maximale.
-Il effectue ces opérations : 
--On initialise la somme maximale à moins l'infini et les indices du résultat à None.
--On utilise des boucles imbriquées pour parcourir tous les indices possibles de i1, i2, j1, et j2.
--Pour chaque combinaison d'indices, on calcule la somme de la sous-matrice correspondante en utilisant la bibliothèque NumPy.
--Si une somme plus grande est trouvée, on met à jour la somme maximale et les indices du résultat.
--En fin de compte, on renvoie la somme maximale et les indices correspondant
+Pour cette algorithme j'ai d'abord initialiser les variables dont j'avais besoin puis j'ai parcouru la matrice en comparant les differents sommes entre elles afin de ne garder que la somme la plus importante 
+Puis j'ai effectué un affichage de la somme ainsi que de la sous-matrice
